1b0f3ea504499b931c138951ccec8aa05aefae4f
Jannik <jannik.sass@gmx.net>
2020-01-22 14:21:44 +0100

    Verbesserung

--- python/kommisch.py
+++ python/kommisch.py
@@ -1,18 +1,12 @@
 
-for i in range(10):
-    x=int(input("Gib eine zahl an von der du die   wissen willst? "))
 
+def fce(x):
     y=x-1
     s=x*y
     x=y
-
-    while y>1:
-            
+    while y>1:      
         y=x-1
         s=s*y
         x=y
-
-            
-
-    print(s)
+    print("Das Ergebnis ist:",s)
 
