d1f1298cfb2e4d195a0338900213d5939ccf0d88
Jannik <jannik.sass@gmx.net>
2020-01-22 12:33:47 +0100

    Primenzahlen finder hinzugefügt

---
+++ python/primenzahlen.py
@@ -0,0 +1,19 @@
+x=2
+
+if int(x)==x:
+    print("W")
+x=2
+y=3
+for i in range(1000):
+    n=y/x
+
+    if int(n)==n:
+        y=y+1
+        x=2
+    elif int(n)!=n:
+        x=x+1
+
+    elif x==y:
+        print(y)
+
+    
